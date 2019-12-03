be8baf1955cab3c8e93c9664aa99855cbcb68105
Nadim Mahmud <mahmudnadim16@gmail.com>
2019-12-03 11:57:47 +0600

    Update Case_Conversion_Ignoring_Spacial_Char.asm

--- Case_Conversion_Ignoring_Spacial_Char.asm
+++ Case_Conversion_Ignoring_Spacial_Char.asm
@@ -1,51 +1,113 @@
+
 .MODEL SMALL
-.STACK 100H
-.CODE
+.STACK 100H    
 
-MAIN PROC
+.DATA
+str DB 80 DUP
 
-    ; 1 will request input
-    
-MOV AH,1
-INT 21H  
-    ; storing input into temporary register
-MOV BL,AL
+.CODE   
+MOV AX, @DATA
+MOV DS, AX
 
-    ; converting upper to lower and vice-versa
+MAIN PROC    
+           
+           
+MOV BL,48           
+MOV CL, 48
+MOV AH, 1 
 
-CMP BL,'z'
-    ; ignoring spacial charecter
+NEXT:
+INT 21H                   
+CMP AL, 0DH
+JZ EXIT
+
+CMP AL,'z'
 JG ignore  
-CMP BL,'A'
+CMP AL,'A'
 JL ignore
 
+CMP AL,96
+JG  OK
 
-CMP BL,96
-JG  upper
-
-CMP BL,90
+CMP AL,90
 JG  ignore
 
-ADD BL,64
-upper:
-SUB BL,32
-         
+OK:
+
+MOV str[SI], AL
+
+
+CMP str[SI],'A'  
+JZ vowel
+CMP str[SI],'a'
+JZ vowel
+CMP str[SI],'E'
+JZ vowel
+CMP str[SI],'e'
+JZ vowel
+CMP str[SI],'I'
+JZ vowel
+CMP str[SI],'i'
+JZ vowel
+CMP str[SI],'O'
+JZ vowel       
+CMP str[SI],'o'
+JZ vowel
+CMP str[SI],'U'
+JZ vowel
+CMP str[SI],'u'
+JZ vowel       
+       
+       
+ADD CL,1
+SUB BL,1
+vowel:
+ADD BL,1
+
 ignore:
-    ;ADD DL,-10
-    ; PRINTING NEW LINE (actually printing 80 chars)
-    ; setting for output request
+
+INC SI
+JMP NEXT            
+EXIT:                 
+
+DEC SI
+            
+MOV AH,2 
+MOV DL,0AH
+INT 21H
+MOV DL,0DH
+INT 21H
+            
+
+;OUTPUT:
+;MOV DL, str[SI]
+;INT 21H       
+;DEC SI
+;CMP SI, -1
+;JZ SESH 
+;JMP OUTPUT
+
+;SESH:
+ 
+ 
+MOV AH,2
+MOV DL,BL
+INT 21H   
+
 MOV AH,2 
 MOV DL,0AH
 INT 21H
 MOV DL,0DH
 INT 21H
 
-MOV DL,BL
 MOV AH,2
-INT 21H
+MOV DL,CL
+INT 21H  
+           
+              
+MOV AH, 4CH
+INT 21H     
 
-MOV AH,4CH  ;eat extra printer char
-INT 21H
+MAIN ENDP     
 
-MAIN ENDP
 END MAIN
